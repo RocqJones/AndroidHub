15b2d9f9d0547fc76598554c58f7cbfe0cd57138
MrFinch <intoverflown@localhost.localdomain>
2019-03-11 13:11:06 +0300

    Grid Layout

---
+++ GridLayoutDemo/app/src/main/java/com/rocqjones/intoverflown/gridlayoutdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.rocqjones.intoverflown.gridlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void btn1(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
