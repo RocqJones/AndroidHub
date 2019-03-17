8e05d694ecc9826a7c310ebd193261c1ba329c43
MrFinch <intoverflown@localhost.localdomain>
2019-03-17 16:14:47 +0300

    3.2 Project Mkulima Mrkt

--- MkulimaMarket/app/src/main/java/com/rocqjones/intoverflown/mkulimamarket/MainActivity.java
+++ MkulimaMarket/app/src/main/java/com/rocqjones/intoverflown/mkulimamarket/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -22,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
                 pyrethrum, sugarcane, sunflower, poutry;
     //GridLayout id
     GridLayout grid;
+    //Buying and Selling RelativeLayout Id
+    RelativeLayout buyingAndSellingRelView, buyersRelativeLayout, sellersRelativeLayout;
+    //Buying and Selling Button Id
+    Button buying, selling;
 
     private TextView mTextMessage;
 
@@ -36,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     //hiding gridLayout on homepage
                     grid.setVisibility(View.INVISIBLE);
+                    buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+                    buyersRelativeLayout.setVisibility(View.INVISIBLE);
+                    sellersRelativeLayout.setVisibility(View.INVISIBLE);
                     return true;
 
                 //Dashboard case
@@ -44,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
                     grid.setVisibility(View.VISIBLE);
                     mTextMessage.setVisibility(View.INVISIBLE);
                     //mTextMessage.setText(R.string.title_dashboard);
+                    buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+                    buyersRelativeLayout.setVisibility(View.INVISIBLE);
+                    sellersRelativeLayout.setVisibility(View.INVISIBLE);
                     return true;
 
                 //Notification case
@@ -51,12 +63,18 @@ public class MainActivity extends AppCompatActivity {
                     //hiding gridLayout on notification
                     grid.setVisibility(View.INVISIBLE);
                     mTextMessage.setText(R.string.title_notifications);
+                    buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+                    buyersRelativeLayout.setVisibility(View.INVISIBLE);
+                    sellersRelativeLayout.setVisibility(View.INVISIBLE);
                     return true;
 
                 //Message case
                 case R.id.navigation_message:
                     grid.setVisibility(View.INVISIBLE);
                     mTextMessage.setText(R.string.title_Messages);
+                    buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+                    buyersRelativeLayout.setVisibility(View.INVISIBLE);
+                    sellersRelativeLayout.setVisibility(View.INVISIBLE);
                     return true;
             }
             return false;
@@ -91,6 +109,16 @@ public class MainActivity extends AppCompatActivity {
 
         //For GridLayout id
         grid = (GridLayout) findViewById(R.id.gridLId);
+
+        //Call id for relative view on Buying and Selling screen
+        buyingAndSellingRelView = (RelativeLayout) findViewById(R.id.buyingAndSellingRelView);
+        buying = (Button) findViewById(R.id.buying);
+        selling = (Button) findViewById(R.id.selling);
+
+        //Buyers layout
+        buyersRelativeLayout = (RelativeLayout) findViewById(R.id.buyersRelativeLayout);
+        //Sellers Layout
+        sellersRelativeLayout = (RelativeLayout) findViewById(R.id.sellersRelativeLayout);
     }
 
     @Override
@@ -118,78 +146,177 @@ public class MainActivity extends AppCompatActivity {
     //PRODUCT START (onClick) - Customization of each product ImageButton
     public void maize(View view){
         Toast.makeText(getApplicationContext(), "maize", Toast.LENGTH_LONG).show();
+        //set previous grid to invisible
+        grid.setVisibility(View.INVISIBLE);
+        //set Relative layout Visible
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        //Buying Layout
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        //selling Layout
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void beans(View view){
         Toast.makeText(getApplicationContext(), "beans", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void cowpeas(View view){
         Toast.makeText(getApplicationContext(), "cowpeas", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void tomatoes(View view){
         Toast.makeText(getApplicationContext(), "tomatoes", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void potatoes(View view){
         Toast.makeText(getApplicationContext(), "potatoes", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void onions(View view){
         Toast.makeText(getApplicationContext(), "onions", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void fruits(View view){
         Toast.makeText(getApplicationContext(), "fruits", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void coconuts(View view){
         Toast.makeText(getApplicationContext(), "coconuts", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void othernuts(View view){
         Toast.makeText(getApplicationContext(), "othernuts", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void carrots(View view){
         Toast.makeText(getApplicationContext(), "carrots", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public void greenpeas(View view){
         Toast.makeText(getApplicationContext(), "greenpeas", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void fish(View view){
         Toast.makeText(getApplicationContext(), "fish", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void coffee(View view){
         Toast.makeText(getApplicationContext(), "coffee", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void tea(View view){
         Toast.makeText(getApplicationContext(), "tea", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void rice(View view){
         Toast.makeText(getApplicationContext(), "rice", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void pyrethrum(View view){
         Toast.makeText(getApplicationContext(), "pyrethrum", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void sugarcane(View view){
         Toast.makeText(getApplicationContext(), "sugarcane", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void sunflower(View view){
         Toast.makeText(getApplicationContext(), "sunflower", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
 
     public  void poutry(View view){
         Toast.makeText(getApplicationContext(), "poutry", Toast.LENGTH_LONG).show();
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.VISIBLE);
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
     }
     //END OF PRODUCTS
+
+    //BEGINING of Buying and Selling
+    public void buying(View view) {
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+        //set Toast onClick
+        Toast.makeText(getApplicationContext(), "Buying", Toast.LENGTH_LONG).show();
+        buyersRelativeLayout.setVisibility(View.VISIBLE);
+        sellersRelativeLayout.setVisibility(View.INVISIBLE);
+    }
+
+    public void selling(View view) {
+        grid.setVisibility(View.INVISIBLE);
+        buyingAndSellingRelView.setVisibility(View.INVISIBLE);
+        //set Toast onClick
+        Toast.makeText(getApplicationContext(), "Selling", Toast.LENGTH_LONG).show();
+        buyersRelativeLayout.setVisibility(View.INVISIBLE);
+        sellersRelativeLayout.setVisibility(View.VISIBLE);
+    }
 }
